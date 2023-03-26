d2c32e4391bf9b19c8f31fcae82b4d6ba2c52105
Lukáš Vožda <vozdalukas@gmail.com>
2023-03-26 21:41:13 +0200

    Added info to about page

--- src/frontend/src/routes/about/+page.svelte
+++ src/frontend/src/routes/about/+page.svelte
@@ -1,3 +1,46 @@
-<div class="w-5/6 md:w-3/4 mx-auto">
-  <h2>About page</h2>
+<div class="hero min-h-screen">
+  <div class="hero-content text-center">
+    <div class="max-w-md">
+      <h1 class="text-5xl font-bold py-8">About the project</h1>
+      <p class="py-6">
+        Bitshop if a proof of concept project that demonstrates capabilities of the <a class="link" href="https://internetcomputer.org">Internet
+        Computer blockchain.</a> It is a Shopify alternative <b>running 100 % on-chain with Bitcoin as a
+        native payment.</b> There is no need to install any additional software to use this app – it is
+        all approachable via web browser.
+      </p>
+
+      <p class="py-6">
+        We implemented CRUD support for products, categories, orders. You can also upload images for
+        each product that are also stored on-chain.
+      </p>
+      <p class="py-6">
+        Unique BTC address is generated for each order – the backend is able to verify the
+        payment automatically.
+      </p>
+      <p class="py-6">
+        We used Sveltekit to build the frontend of this app and the backend is written in the Motoko
+        language, designed to write smart contracts on the IC. Source code is available in this GitHub repo with <b>open source</b> license.
+      </p>
+      <div><a class="link link-primary" href="https://github.com/lukasvozda/bitshop">https://github.com/lukasvozda/bitshop</a></div>
+      <p class="py-6">
+        These features are missing in order to be able to run an online store on Bitshop in real
+        world:
+      </p>
+      <ul class="list-disc">
+        <li>Product Variants</li>
+        <li>E-mail confirmations</li>
+        <li>Shipping providers integrations</li>
+        <li>More payment methods (USDC, ETH, VISA)</li>
+        <li>Support for discounts</li>
+        <li>Tax and invoices</li>
+        <li>Multiple images support</li>
+      </ul>
+      <p class="py-6">If you would like to use Bitshop for your own projects, <b>please let us know what features are most
+        important to you.</b>
+        </p>
+        <div><a class="link link-primary" href="mailto:contact@bitshop.info">contact@bitshop.info</a></div>
+        <div><a class="link link-primary" href="https://twitter.com/bitshopicp">https://twitter.com/bitshopicp</a></div>
+    </div>
+  </div>
 </div>
+
